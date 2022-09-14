0cca57291a435ee005bcfa32368e90802abac874
坂田海斗 <66310212+kaito0902@users.noreply.github.com>
2022-09-15 00:59:58 +0900

    アカウント登録画面作成

--- app/src/main/java/com/sghh/android/MainActivity.java
+++ app/src/main/java/com/sghh/android/MainActivity.java
@@ -2,13 +2,28 @@ package com.sghh.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mRegister_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRegister_button = findViewById(R.id.register_button);
+
+        mRegister_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
