c5736903a0fc8836d00a9e113dcffdedfbb925c2
kaito0902 <66310212+kaito0902@users.noreply.github.com>
2022-09-18 13:31:27 +0900

    連絡先、カレンダーのアクセス権の追加

--- app/src/main/java/com/sghh/android/MainActivity.java
+++ app/src/main/java/com/sghh/android/MainActivity.java
@@ -1,11 +1,15 @@
 package com.sghh.android;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,12 +22,26 @@ public class MainActivity extends AppCompatActivity {
 
         mRegister_button = findViewById(R.id.register_button);
 
-        mRegister_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
-                startActivity(intent);
+        mRegister_button.setOnClickListener(view -> {
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CALENDAR) == PackageManager.PERMISSION_GRANTED) {
+                startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
+            } else {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requestPermissions(new String[]{Manifest.permission.READ_CONTACTS, Manifest.permission.READ_CALENDAR}, 0);
+                }
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 0) {
+            if (grantResults.length > 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
+            }
+        }
     }
 }
