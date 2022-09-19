e560f99f7c70c50bc086194e1f1c26dc418de9bc
kaito0902 <66310212+kaito0902@users.noreply.github.com>
2022-09-19 18:09:36 +0900

    メールアドレス欄の入力補完機能追加

--- app/src/main/java/com/sghh/android/RegistrationActivity.java
+++ app/src/main/java/com/sghh/android/RegistrationActivity.java
@@ -1,14 +1,27 @@
 package com.sghh.android;
 
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class RegistrationActivity extends AppCompatActivity {
+
+    private static final String[] MAILS = new String[]{
+            "abcd@abcd.com", "efgh@efgh.com", "ijkl@ijklcom", "mnop@mnop.com",
+            "aaa@aaa.com", "aaaa@aaaa.com", "aaaaa@aaaaa.com"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_dropdown_item_1line, MAILS);
+        AutoCompleteTextView mail_view = findViewById(R.id.edit_email_address);
+
+        mail_view.setAdapter(adapter);
     }
 }
