5b7bafc55f973cee13c322173c6271a166b6f0a2
kaito0902 <66310212+kaito0902@users.noreply.github.com>
2022-09-22 02:25:52 +0900

    getAccountsByTypeでgoogleアカウントが取得できない

--- app/src/main/java/com/sghh/android/RegistrationActivity.java
+++ app/src/main/java/com/sghh/android/RegistrationActivity.java
@@ -1,6 +1,9 @@
 package com.sghh.android;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 
@@ -8,18 +11,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class RegistrationActivity extends AppCompatActivity {
 
-    private static final String[] MAILS = new String[]{
-            "abcd@abcd.com", "efgh@efgh.com", "ijkl@ijklcom", "mnop@mnop.com",
-            "aaa@aaa.com", "aaaa@aaaa.com", "aaaaa@aaaaa.com"
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_dropdown_item_1line, MAILS);
+        AccountManager am = AccountManager.get(this);
+        Account[] accounts = am.getAccountsByType("com.google");
+
+        ArrayAdapter<Account> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_dropdown_item_1line, accounts);
         AutoCompleteTextView mail_view = findViewById(R.id.edit_email_address);
 
         mail_view.setAdapter(adapter);
