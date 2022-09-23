4a7197dfb958ff9913994132ca7e5905e2a1cdc5
kaito0902 <66310212+kaito0902@users.noreply.github.com>
2022-09-23 17:26:25 +0900

    メールアドレス欄の入力補完機能をgmailに変更

--- app/src/main/java/com/sghh/android/RegistrationActivity.java
+++ app/src/main/java/com/sghh/android/RegistrationActivity.java
@@ -3,13 +3,16 @@ package com.sghh.android;
 import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class RegistrationActivity extends AppCompatActivity {
+
+    ArrayList<String> mArrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +22,13 @@ public class RegistrationActivity extends AppCompatActivity {
         AccountManager am = AccountManager.get(this);
         Account[] accounts = am.getAccountsByType("com.google");
 
-        ArrayAdapter<Account> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_dropdown_item_1line, accounts);
+        // mArrayListにaccountsのnameを格納
+        for (int i = 0; i < accounts.length; i++) {
+            mArrayList.add(accounts[i].name);
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_dropdown_item_1line, mArrayList);
         AutoCompleteTextView mail_view = findViewById(R.id.edit_email_address);
 
         mail_view.setAdapter(adapter);
